cabba608c9a12e983c17771d99c419cb7b46dc46
DCoetzee22 <danet.coetzee@gmail.com>
2022-07-14 14:24:13 +0200

    Add files via upload
    First commit

---
+++ Main.js
@@ -0,0 +1,13 @@
+/* 
+ Author: Danette Coetzee
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+          var userName = prompt("What is your name?")
+          
+          function sayHello(){
+              document.write("Hello, " + userName);
+          }
+          
+          sayHello();
